3ac23245bcb575f73771ec88701e2135a267c02e
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-08-28 11:17:05 -0600

    feature: add secret version id validation

--- tests/testimpl/test_impl.go
+++ tests/testimpl/test_impl.go
@@ -14,18 +14,20 @@ import (
 )
 
 func TestSecretsManagerComplete(t *testing.T, ctx types.TestContext) {
-	// TODO: Remove this test from your module once you have defined some other tests.
 	t.Run("TestARNAndIDPatternMatches", func(t *testing.T) {
-		checkARNIDFormat(t, ctx)
+		checkARNAndIDFormat(t, ctx)
 	})
 
 	t.Run("TestingSecretExists", func(t *testing.T) {
 		testSecretsManager(t, ctx)
 	})
 
+	t.Run("CheckSecretVersionId", func(t *testing.T) {
+		checkSecretVersion(t, ctx)
+	})
 }
 
-func checkARNIDFormat(t *testing.T, ctx types.TestContext) {
+func checkARNAndIDFormat(t *testing.T, ctx types.TestContext) {
 	expectedPatternARN := `^arn:aws:secretsmanager:[a-z0-9-]+:\d{12}:secret:[a-zA-z0-9/_+=.@-]+$`
 
 	actualID := terraform.Output(t, ctx.TerratestTerraformOptions(), "id")
@@ -35,6 +37,8 @@ func checkARNIDFormat(t *testing.T, ctx types.TestContext) {
 	actualARN := terraform.Output(t, ctx.TerratestTerraformOptions(), "arn")
 	assert.NotEmpty(t, actualARN, "ARN is empty")
 	assert.Regexp(t, expectedPatternARN, actualARN, "ARN does not match expected pattern")
+
+	assert.Regexp(t, actualARN, actualID, "ARN doesn't match with ID")
 }
 
 func testSecretsManager(t *testing.T, ctx types.TestContext) {
@@ -54,6 +58,26 @@ func testSecretsManager(t *testing.T, ctx types.TestContext) {
 	assert.Equal(t, expectedARN, *actualARN, "Secret ARN doesn't match")
 }
 
+func checkSecretVersion(t *testing.T, ctx types.TestContext) {
+	input := &secretsmanager.DescribeSecretInput{
+		SecretId: aws.String(terraform.Output(t, ctx.TerratestTerraformOptions(), "arn")),
+	}
+
+	client := GetSecretsManagerClient(t)
+
+	result, err := client.DescribeSecret(context.TODO(), input)
+	assert.NoError(t, err, "The expected secret was not found")
+
+	VersionIdsToStages := result.VersionIdsToStages
+
+	currentSecretVersionId, err := GetCurrentSecretVersion(VersionIdsToStages)
+	assert.NoError(t, err)
+
+	expectedSecretVersionId := terraform.Output(t, ctx.TerratestTerraformOptions(), "secret_version_id")
+	assert.NoError(t, err)
+	assert.Equal(t, currentSecretVersionId, expectedSecretVersionId, "Actual secret id and expected doesn't match")
+}
+
 func GetAWSConfig(t *testing.T) (cfg aws.Config) {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	require.NoErrorf(t, err, "unable to load SDK config, %v", err)
@@ -63,4 +87,15 @@ func GetAWSConfig(t *testing.T) (cfg aws.Config) {
 func GetSecretsManagerClient(t *testing.T) *secretsmanager.Client {
 	secretsManagerClient := secretsmanager.NewFromConfig(GetAWSConfig(t))
 	return secretsManagerClient
+}
+
+func GetCurrentSecretVersion(VersionIdsToStages map[string][]string) (string, error) {
+	for versionId, stages := range VersionIdsToStages {
+		for _, stage := range stages {
+			if stage == "AWSCURRENT" {
+				return versionId, nil
+			}
+		}
+	}
+	return "", nil
 }
